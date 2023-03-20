0c1a6915f39b0b1afdef327a508a75fc313e392c
Malcottan <maxtryasiy@gmail.com>
2023-03-20 11:12:46 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    float x;
+    cout << "x" << endl;
+    cout << "Enter the number: ";
+    cin >> x;
+    cout << "Output number: " << pow(x,2) << endl;
+    return 0;
+}
